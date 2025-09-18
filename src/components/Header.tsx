adc3a0988437e49b13877ecace71132aea9c2ebf
Mohamed Moustafa <hi@fairewinds.ca>
2025-09-18 13:58:07 +0200

    Cleanup boilerplate + add Tailwind

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import viteLogo from '/vite.svg'
+import reactLogo from '../assets/react.svg'
+
+function Header() {
+  return (
+    <>
+      <header className="flex items-center space-x-8 px-16 py-8">
+        <h1 className="text-3xl">Fresnel</h1>
+      </header>
+    </>
+  )
+}
+
+export default Header
