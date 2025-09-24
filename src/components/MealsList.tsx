519b057369b1987e0226ea7229c5c98a33a2454b
Mohamed Moustafa <hi@fairewinds.ca>
2025-09-24 14:59:39 +0200

    Add loading and empty states to meals list

--- src/components/MealsList.tsx
+++ src/components/MealsList.tsx
@@ -1,47 +1,51 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import MealsListItem from './MealsListItem.tsx'
-
-async function fetchMealsList () {
-  let result = []
-
-  try {
-    const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/meals`)
-
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`)
-    }
-
-    result = await response.json()
-    console.log(result)
-  } catch (error) {
-    console.error(error.message)
-  } finally {
-    return result
-  }
-}
 
 export default function MealsList() {
   const [mealsList, setMealsList] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    setMealsList(fetchMealsList())
+    fetchMealsList()
   }, [])
+
+  async function fetchMealsList () {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/meals`)
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`)
+      }
+
+      const result = await response.json()
+
+      setMealsList(result.meals)
+      setIsLoading(false)
+      console.log(result)
+
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
 
   return (
     <div>
       <h2 className="text-2xl mb-8">Meals</h2>
 
-      <ul className="flex flex-col gap-8">
-        {mealsList.map(meal => (
-          <MealsListItem
-            key={meal.id}
-            name={meal.name}
-            mealTypes={meal.mealTypes}
-            difficulty={meal.difficulty}
-            leftoverable={meal.leftoverable}
-          />
-        ))}
-      </ul>
+      {isLoading && <p>Loading...</p>}
+
+      {mealsList.length === 0 ? <p>No meals yet.</p> :
+        <ul className="flex flex-col gap-8">
+          {mealsList.map(meal => (
+            <MealsListItem
+              key={meal.id}
+              name={meal.name}
+              mealTypes={meal.mealTypes}
+              difficulty={meal.difficulty}
+              leftoverable={meal.leftoverable}
+            />
+          ))}
+        </ul>}
     </div>
   )
 }
